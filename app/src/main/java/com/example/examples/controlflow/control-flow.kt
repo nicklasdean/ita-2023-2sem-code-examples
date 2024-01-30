02791495ba70f18485c81c89c99ddd8e7348b2a4
dean <nicklasfrederiksen@gmail.com>
2024-01-30 10:17:10 +0100

    first

--- app/src/main/java/com/example/examples/controlflow/control-flow.kt
+++ app/src/main/java/com/example/examples/controlflow/control-flow.kt
@@ -1,5 +1,17 @@
 package com.example.examples.controlflow
 
-fun main(){
+import kotlin.random.Random
+
+fun main() {
     println("Hello World");
 }
+
+//A function that determines whether a person is 18 and is a member
+
+//A function that determines the season
+
+//A function that displays an iterative loop
+
+//Finding the sum of a list of numbers
+
+
--- app/src/main/java/com/example/examples/lambdaexpressions/lambda-expressions.kt
+++ app/src/main/java/com/example/examples/lambdaexpressions/lambda-expressions.kt
@@ -1,5 +1,25 @@
 package com.example.examples.lambdaexpressions
 
 fun main() {
-    println("Hello World");
+
+}
+
+//Filtering a list of numbers with a function reference
+
+//Filtering a list of numbers with an anonymous lambda function
+
+
+// Higher order function example: Calculator
+// This is the higher order function that takes a function as parameter (2 ints and returns an Int)
+fun calculate(x: Int, y: Int, operation: (Int, Int) -> Int): Int {
+    return operation(x, y)
+}
+fun add(x: Int, y: Int): Int {
+    return x + y
+}
+fun subtract(x: Int, y: Int): Int {
+    return x - y
+}
+fun multiply(x: Int, y: Int): Int {
+    return x * y
 }
