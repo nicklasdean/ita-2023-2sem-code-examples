169b9a28d594157c46cbac6bd24417aec3a2522c
dean <nicklasfrederiksen@gmail.com>
2024-02-02 14:40:14 +0100

    first

--- app/src/main/java/com/example/examples/controlflow/control-flow.kt
+++ app/src/main/java/com/example/examples/controlflow/control-flow.kt
@@ -5,9 +5,50 @@ fun main() {
 }
 
 //A function that determines whether a person is above 18 and is a member
+fun isAdultAndMember(age: Int, member: Boolean): Boolean {
+    if (age >= 18 && member == true) {
+        return true;
+    }
+    return false;
+}
 
 //A function that determines the season
+fun season(): Unit {
+    println("Enter season");
+    val input: Int = readln().toInt();
+    when (input) {
+        12, 1, 2 -> {
+            println("Winter");
+        }
+        3, 4, 5 -> {
+            println("Spring");
+        }
+        6, 7, 8 -> {
+            println("Summer");
+        }
+        9, 10, 11 -> {
+            println("Fall");
+        }
+        else -> {
+            println("Invalid input");
+        }
+    }
+}
 
 //A function that displays an iterative loop
+fun loop(): Unit {
+    for (i in 1..5) {
+        println(i);
+    }
+}
 
 //Finding the sum of a list of numbers
+fun sum(): Int {
+    val list: List<Int> = listOf(1, 2, 3, 4, 5, 6, 7, 8);
+
+    var sum: Int = 0;
+    for (number: Int in list) {
+        sum += number;
+    }
+    return sum;
+}
--- app/src/main/java/com/example/examples/lambdaexpressions/lambda-expressions.kt
+++ app/src/main/java/com/example/examples/lambdaexpressions/lambda-expressions.kt
@@ -1,17 +1,26 @@
 package com.example.examples.lambdaexpressions
 
-fun main() {
-}
-
-
 //Filtering a list of numbers with function reference
 fun filterWithFunction(): Unit {
+    val list: List<Int> = listOf(1,2,3,4,5,6,7,8,9);
+    val filteredList: List<Int> = list.filter(::howToFilter);
+    println(filteredList);
+}
+
+fun howToFilter(number: Int) : Boolean{
+    if(number > 5){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 
 //Filtering a list of numbers with an anonymous lambda function
 fun filterWithLambda() {
-
+    val list: List<Int> = listOf(1,2,3,4,5,6,7,8,9);
+    list.filter { it >= 6 }
 }
 
 // Higher order function example: Calculator
@@ -27,4 +36,12 @@ fun add(x: Int, y: Int): Int {
 
 fun subtract(x: Int, y: Int): Int {
     return x - y
+}
+
+fun product(x: Int, y:Int): Int{
+    return x * y;
+}
+
+fun main() {
+    //println(calculate(2, 2, {}));
 }
